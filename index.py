1024aaaceb9bd6d295d69d7df6827304e033ea13
Thomas Gilgenast <thomasgilgenast@gmail.com>
2021-10-19 18:30:55 -0400

    lint

--- index.py
+++ index.py
@@ -4,15 +4,23 @@ import subprocess
 import ipywidgets as widgets
 
 
-uploader = widgets.FileUpload(accept='', multiple=False)
+uploader = widgets.FileUpload(accept="", multiple=False)
 vbox = widgets.VBox([uploader])
+
 
 def on_change(change):
     global vbox
     filename = list(uploader.value.keys())[0]
-    original_image = uploader.value[filename]['content']
-    inverted_image = subprocess.run('convert -channel RGB -negate - -', input=original_image, capture_output=True, shell=True, check=True).stdout
+    original_image = uploader.value[filename]["content"]
+    inverted_image = subprocess.run(
+        "convert -channel RGB -negate - -",
+        input=original_image,
+        capture_output=True,
+        shell=True,
+        check=True,
+    ).stdout
     vbox.children = [uploader, widgets.Image(value=inverted_image)]
 
-uploader.observe(on_change, 'value')
+
+uploader.observe(on_change, "value")
 vbox
