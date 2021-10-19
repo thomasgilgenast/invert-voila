20b8faf5aba57d0bea36d4fcf84826890d9766d5
Thomas Gilgenast <thomasgilgenast@gmail.com>
2021-10-19 18:08:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+invert-voila
+============
+
+A voila app to invert images using imagemagick.
+
+Quickstart
+----------
+
+    conda env create
+    conda activate invert-voila
+    jupytext --to ipynb index.py --set-kernel -
+    voila --theme dark index.ipynb
---
+++ index.py
@@ -0,0 +1,18 @@
+# %%
+import subprocess
+
+import ipywidgets as widgets
+
+
+uploader = widgets.FileUpload(accept='', multiple=False)
+vbox = widgets.VBox([uploader])
+
+def on_change(change):
+    global vbox
+    filename = list(uploader.value.keys())[0]
+    original_image = uploader.value[filename]['content']
+    inverted_image = subprocess.run('convert -channel RGB -negate - -', input=original_image, capture_output=True, shell=True, check=True).stdout
+    vbox.children = [uploader, widgets.Image(value=inverted_image)]
+
+uploader.observe(on_change, 'value')
+vbox
